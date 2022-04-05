4625594032e4276ff0c8258f6aaa56e0be6244ab
Eduard <vercaemereduard@gmail.com>
2022-04-05 17:28:49 +0000

    Endpoint for user login

--- api/src/endpoints/user.js
+++ api/src/endpoints/user.js
@@ -1,3 +1,4 @@
+const jwt = require("jsonwebtoken");
 const db = require("../db");
 
 const RESULT = {
@@ -5,6 +6,8 @@ const RESULT = {
     INVALID_STRING: "INVALID_STRING",
     USERNAME_TAKEN: "USERNAME_TAKEN",
     USER_CREATED: "USER_CREATED",
+    BAD_CREDENTIALS: "BAD_CREDENTIALS",
+    GENERATED_TOKEN: "GENERATED_TOKEN",
 };
 
 function finish({ req, next, code, data }) {
@@ -23,7 +26,7 @@ function validateString(field) {
     return function (req, res, next) {
         const item = req.body[field];
         if (!item || typeof item !== "string")
-            return finish({ req, next, code: RESULT.INVALID_STRING, data: { field }});
+            return finish({ req, next, code: RESULT.INVALID_STRING, data: { field } });
         next();
     }
 }
@@ -54,7 +57,26 @@ function persistUser(req, res, next) {
 }
 
 function loginUser(req, res, next) {
-    next(new Error("UNIMPLEMENTED"));
+    const { username, password } = req.body;
+    db.Player.findOne({
+        where: {
+            name: username,
+            password,
+        }
+    })
+        .then(player => {
+            if (player) next();
+            else finish({ req, next, code: RESULT.BAD_CREDENTIALS });
+        })
+        .catch(next);
+}
+
+function generateToken(req, res, next) {
+    const { username } = req.body;
+    jwt.sign({ username }, "USER_LOGIN_SECRET", (err, token) => {
+        if (err) next(err);
+        else finish({ req, next, code: RESULT.GENERATED_TOKEN, data: { token } });
+    });
 }
 
 function handler(err, req, res, next) {
@@ -63,16 +85,21 @@ function handler(err, req, res, next) {
     }
 
     switch (req.code) {
-    case RESULT.USERNAME_TAKEN:
-        return res.status(200).json({ status: "TAKEN" });
-    case RESULT.USER_CREATED:
-        return res.status(200).json({ status: "CREATED" });
-    case RESULT.INVALID_BODY:
-    case RESULT.INVALID_STRING:
-        return res.status(400).send('Bad client data');
-    default:
-        console.error('Unhandled server exception: ', err);
-        return res.status(500).send(req.code);
+        case RESULT.USERNAME_TAKEN:
+            return res.status(200).json({ status: "TAKEN" });
+        case RESULT.USER_CREATED:
+            return res.status(200).json({ status: "SUCCESS" });
+        case RESULT.BAD_CREDENTIALS:
+            return res.status(200).json({ status: "BAD_CREDENTIALS" });
+        case RESULT.GENERATED_TOKEN:
+            const { token } = req.data;
+            return res.status(200).json({ status: "SUCCESS", token });
+        case RESULT.INVALID_BODY:
+        case RESULT.INVALID_STRING:
+            return res.status(400).send('Bad client data');
+        default:
+            console.error('Unhandled server exception: ', err);
+            return res.status(500).send(req.code);
     }
 }
 
@@ -91,6 +118,7 @@ module.exports = {
         validateString("username"),
         validateString("password"),
         loginUser,
+        generateToken,
         handler,
     ],
 };
--- api/src/index.js
+++ api/src/index.js
@@ -1,14 +1,14 @@
 const db = require("./db");
 const express = require("express");
-
 const user = require("./endpoints/user");
 
 async function main() {
-    await db.sync();  
+    //await db.sync();  
 
     express()
         .use(express.json())
         .post("/user/create", user.create)
+        .post("/user/login", user.login)
         .listen(8081, () => console.log("listening..."));
 }
 
