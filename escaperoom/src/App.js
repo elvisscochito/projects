2d22d9852fa389b5cf5e01c394c90d51f9b00780
Elviro Dominguez Soriano <elvirodominguezsoriano@outlook.com>
2022-04-05 09:09:13 -0500

    Update App.js

--- escaperoom/src/App.js
+++ escaperoom/src/App.js
@@ -21,7 +21,7 @@ function App() {
   return (
     <div className="App">
 
-      {/* <Profile profileName={"profileName"}/> */}
+      <Profile profileName={"profileName"}/>
 
       <BrowserRouter>
         <Routes>
