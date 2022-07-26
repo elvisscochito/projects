c4f1413ab18ca342cecc043d3a3f39b89f3f63f4
Elviro Dominguez Soriano <elvirodominguezsoriano@outlook.com>
2022-07-25 23:26:18 -0500

    vercel
    vercel files was added

---
+++ markdown-blog/index.js
@@ -0,0 +1,49 @@
+import express from 'express';
+
+import mongoose from 'mongoose';
+import methodOverride from 'method-override';
+
+import articleRouter from './routes/articles.js';
+import Article from './models/article.js';
+
+const hostname = process.env.HOSTNAME || 'localhost';
+const port = process.env.PORT || 3000;
+const server = express();
+
+mongoose.connect('mongodb://localhost/markdown-blog');
+
+server.set('port', port);
+server.set('hostname', hostname);
+server.set('view engine', 'ejs');
+
+server.use(express.urlencoded({ extended: false }));
+/** @note overload the method of the form */
+server.use(methodOverride('_method'));
+
+server.get('/', (req, res) => {
+    res.send("Hello vercel");
+});
+
+server.get('/home', async (req, res) => {
+    /* const articles = [
+        {
+            title: "Test article",
+            date: new Date(),
+            description: "Test description"
+        },
+        {
+            title: "Test article 2",
+            date: new Date(),
+            description: "Test description 2"
+        }
+    ]; */
+
+    const articles = await Article.find().sort({date: 'desc'});
+    res.render('articles/index.ejs', {articles});
+});
+
+server.use('/articles', articleRouter);
+
+server.listen(server.get('port'), () => {
+    console.log(`Server running at http://${server.get('hostname')}:${server.get('port')}/`);
+});
